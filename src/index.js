db6cd0c3f207bc245883a92f1c4e97d2fa7ddfb0
hitashreekorgaonkar <hkorgaonkar10@gmail.com>
2024-10-17 18:08:12 +0530

    setting up firebase

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-console.log("hello from index.js");
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBm21-Wtc1s4mnLBMpcTlbT8i3eZwLsjb8",
+  authDomain: "fir-tutorial-1477a.firebaseapp.com",
+  projectId: "fir-tutorial-1477a",
+  storageBucket: "fir-tutorial-1477a.appspot.com",
+  messagingSenderId: "587731561551",
+  appId: "1:587731561551:web:06b9d192d84c0d6704ce3a",
+};
+
+// Initialize Firebase
+initializeApp(firebaseConfig);
